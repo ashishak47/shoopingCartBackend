4a362200277b626fa5f0c305a9bbbcc1cbb264c0
harshit sinha <harshit886@outlook.com>
2018-11-27 16:25:55 +0530

    production changes

--- config.js
+++ config.js
@@ -4,5 +4,10 @@ module.exports = {
       saltingRounds: 10,
       mongoDBUrl : "mongodb://localhost:27017/shoppingCart",
       JWT_SECRET : "addjsonwebtokensecretherelikeQuiscustodietipsoscustodes",
+    },
+    production:{
+      port : process.env.PORT || 5200,
+      saltingRounds : 20,
+      mongoDBUrl : "mongodb://harshit886:bittu@886@ds115579.mlab.com:15579/shopping-list"
     }
   }
